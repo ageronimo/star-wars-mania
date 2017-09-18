2286955a7c94d9da32b81959d16c63a05cb08ff9
angela geronimo <angelageroniimo@gmail.com>
2017-09-18 09:48:27 -1000

    pseudo coded solution

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log('Javascript is running...');
 
 // ES5
+
+let filmNum = [1,2,3,4,5,6,7];
+let dataType = ['title', 'director', 'episode', 'producer', 'openining_crawl', 'release date'];
+
 $(document).ready(function(){
 	$('button').click(function(){
 		console.log('processing...');
@@ -10,7 +14,8 @@ $(document).ready(function(){
 			success: function(data) {
 				console.log('Success: Your film data has been gathered!')
 				$('#films').html(`<p> ${JSON.stringify(data)} </p>`);
-				console.log(JSON.stringify(data));
+				console.log('This is your data: ' + JSON.stringify(data));
+				console.log('this is the title: ' + data.title);
 			},
 			error: function() {
 				console.log('ERROR');
@@ -19,6 +24,29 @@ $(document).ready(function(){
 		})
 	})
 })
+
+
+// PSUEDO
+/* 
+foreach number [1-7]
+	ajax url[this]
+		success: foreach
+ 			loop through data targets to inject
+ 			make <p> for
+ 			p.class loop ^
+	
+ 	*/
+
+	
+
+// data to inject:
+//   title
+//   director
+//   episode
+//   producer
+//   opening_crawl
+//   release date
+
 
 //ES6
 const buildHtmlElements = responseData => 'whatever';
